e9e05406aabbf31b2a49a8f0ed1d784eb436c3e3
Edata <Edata@gmail.com>
2024-07-16 19:36:37 +0530

    initial by

---
+++ DEMOTEST/src/com/nt/beans/Utility.java
@@ -0,0 +1,13 @@
+package com.nt.beans;
+
+public class Utility {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=10;
+		int sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
--- DemoTest2/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author Thinkpad
- *
- */
-module DemoTest {
-}
